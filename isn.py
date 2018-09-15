b5a2f80e0a4bd355245140f114b207d863d21eeb
AzodFR <40648473+AzodFR@users.noreply.github.com>
2018-09-15 19:42:48 +0200

    Update isn.py

--- isn.py
+++ isn.py
@@ -1,28 +1,48 @@
 #s!/usr/bin/python
-#Import du module
 from tkinter import *
-#Création de la fonction qui créer la page
+import random
+class basicMob:
+
+	def __init__(self, nom, hp, x, y):
+		
+		self.nom = nom
+		self.hpMob = hp
+		self.loot = random.randint(x, y)
+
 def affichage():
-	#root = la fenetre
+
 	root = Tk()
-	#Création de la "toile" sur la quelle on va afficher des choses
 	canvas = Canvas(root, width=1920, height=1080, background='white')
-	#Séparation en 3 de la fenêtre
 	ligne1 = canvas.create_line(350, 0, 350, 1080)
 	ligne2 = canvas.create_line(350,420,1920,420)
-	#Ajout de couleur aux 3 parties
 	rectan1 = canvas.create_rectangle(0, 0, 351, 1080,fill='grey')
 	rectan2 = canvas.create_rectangle(351, 419, 1920,1080,fill='yellow')
-	rectan3 = canvas.create_rectangle(352, 0, 1920,418,fill='green')
-	canvas.focus_set()
-	#En cours de création
-	canvas.bind("<Key>", clavier)
+	rectan3 = canvas.create_rectangle(352, 0, 1920,418,fill='green',tags="click")
+	canvas.tag_bind("click","<Button-1>",clicked)
 	canvas.pack()
-	print(root.winfo_pointerxy())
-	#Boucle infini qui permet d'afficher tout ça
+
 	root.mainloop()
-#Fonction qui servira de detecter les clicks
-def clavier(event):
-    touche = event.keysym
-#Appelle de la fonction pour lancer la fenêtre
+
+
+
+def clicked(*args):
+	print("+1")
+
 affichage()
+def mob(x):
+	print('Le mob ', x.nom , ' possède ', x.hpMob , 'HP et looteras ', x.loot , ' coins !')
+
+
+fm = basicMob("Gargantua", 100, 5, 20)
+sm = basicMob("Trivador", 250, 50, 300)
+tm = basicMob("Vadot", 1000, 300, 1500)
+listMob = ['fm', 'sm', 'tm']
+listlong = len(listMob)-1
+print(listlong)
+p = random.randint(0,listlong)
+if listMob[p] == "fm":
+	mob(fm)
+elif listMob[p] == "sm":
+	mob(sm)
+elif listMob[p] == "tm":
+	mob(tm)
